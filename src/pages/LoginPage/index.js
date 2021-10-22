3dc3b8584b118a44628d4cbce48033b6ecbdc2df
bourquecharles <charles.bourque96@gmail.com>
2021-10-22 12:08:34 -0400

    Viasat branding

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -402,7 +402,7 @@ const Login = () => {
       i18n={i18n}
       signIn={SignIn}
       loading={loading}
-      logo="assets/OpenWiFi_LogoLockup_DarkGreyColour.svg"
+      logo="assets/Viasat_logo.png"
       loginResponse={loginResponse}
       forgotResponse={forgotResponse}
       fields={fields}
